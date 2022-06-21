b2db10d8f6ce0a1f6038f446195bf169e7bb05a1
Lottie Rugg <lottie.ruggeasey@gmail.com>
2022-06-21 16:47:18 +0100

    component structure for success and setup message

---
+++ src/components/PageContainer/SuccessPageContent/index.js
@@ -0,0 +1,17 @@
+
+const SuccessPageContent= () => {
+    return (
+    < div class = "card__content" >
+        < img className = "card__illustration" src = "../gifti_assets/thumbsUp_illustration.svg" />
+        < div className = "card__textContent" >
+        < div className = "card__header" >
+        < h2 className = "card__title" > Heck Yeah </h2>
+    <p className="card__subTitle">Your event is all set up, use the link below to add more participants</p>
+    <button className="btn btn--primary">Copy Events URL</button>
+    </div>
+</div>
+</div>
+    )
+}
+
+export default SuccessPageContent
