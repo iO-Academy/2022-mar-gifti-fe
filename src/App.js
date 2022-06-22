f21f3f80dc0dc8bf27a5de4cc67b1818b10b8edb
Rich Mead <richard.mead@hotmail.co.uk>
2022-06-22 09:22:42 +0100

    react routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 import './components/Form'
 import Form from "./components/Form";
+import {
+    BrowserRouter,
+    Link,
+    Route,
+    Routes,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <Form />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/:id' element={<ParticipantPage />}/>
+                <Route path='success' element={<SuccessPage/>}/>
+            </Routes>
+        </BrowserRouter>
+        <Form />
 
     </div>
   );
+}
+
+const Home = () => {
+    return <h1>Home</h1>
+}
+
+const ParticipantPage = () => {
+    return <h1>Participant Page</h1>
+}
+
+const SuccessPage = () => {
+    return <h1>Success!</h1>
 }
 
 export default App;
